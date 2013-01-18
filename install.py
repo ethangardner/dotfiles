bbe5cab076639930c30f056895605ded66821bc0
Ethan Gardner <ethan@ethanandjamie.com>
2013-01-18 14:13:25 -0800

    adding eclipse settings files to gitignore

--- install.py
+++ install.py
@@ -8,6 +8,6 @@ files = os.listdir(folder)
 ignored = [".git", "README.md", "install.py"]
 
 for filename in files:
-	if filename not in  ignored:
+	if filename not in ignored:
 		command = 'ln -s ' + folder + os.sep + filename + ' ./' + filename
 		subprocess.Popen(command.split(), stdout=subprocess.PIPE)
