0c84f01d6773e098412ce66921eeb5e17acbe74a
Ethan Gardner <ethan@ethanandjamie.com>
2013-01-21 14:20:16 -0500

    updated install script

--- install.py
+++ install.py
@@ -12,25 +12,27 @@ they can be placed under version control
 
 packages = [
 	"apt-get update",
-	#### Source control
 	"apt-get install -y git-core", 
 	"apt-get install -y subversion",
 	"apt-get install -y libapache2-svn",
-	#### General Utilities
 	"apt-get install -y imagemagick --fix-missing", 
-	#### Python packages
 	"easy_install mechanize", 
 	"easy_install beautifulsoup4",
 	"easy_install markdown",
 	"apt-get install -y python-imaging",
-	#### Java
+	"apt-get install -y ruby-sass",
+	"apt-get install -y ruby-compass",
+	"apt-get install -y ruby-haml",
 	"apt-get install -y openjdk-7-jre"
 ]
 
 if _platform == "linux" or _platform == "linux2":
+	packagelist = ''
 	for package in packages:
-		package = package + ';'
-	subprocess.Popen(package, shell=True)
+		package = package + '; '
+		packagelist = packagelist + package
+	# print packagelist
+	subprocess.Popen(packagelist, shell=True)
 
 folder = '.' + os.sep + '.dotfiles'
 files = os.listdir(folder)
