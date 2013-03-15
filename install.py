2e38b10e70c855698962835cd77849cf5a7b38ba
Ethan Gardner <ethan@ethanandjamie.com>
2013-03-15 11:37:23 -0700

    updated dotfiles again. still some goofy issues going on that are causing system instability

--- install.py
+++ install.py
@@ -35,10 +35,10 @@ packages = [
 	# Java
 	"apt-get install -y openjdk-7-jre",
 	# Node JS
-	"apt-get install -y python-software-properties python g++ make",
-	"add-apt-repository ppa:chris-lea/node.js",
-	"apt-get update",
-	"apt-get install -y nodejs npm",
+	# "apt-get install -y python-software-properties python g++ make",
+	# "add-apt-repository ppa:chris-lea/node.js",
+	# "apt-get update",
+	# "apt-get install -y nodejs npm",
 	# "ln -s /usr/bin/nodejs /usr/bin/node"
 ]
 
